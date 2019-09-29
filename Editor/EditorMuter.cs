7623a20a6c1f2855c84ab5903c9b0a1387cbf554
baba_s <shotababa1206@gmail.com>
2019-09-29 17:51:10 +0900

    first commit

---
+++ Editor/EditorMuter.cs
@@ -0,0 +1,37 @@
+﻿using System.Reflection;
+using UnityEditor;
+using UnityEngine.Events;
+
+namespace KoganeUnityLib
+{
+	internal static class EditorApplicationUtility
+	{
+		private const BindingFlags BINDING_ATTR =
+			BindingFlags.Static |
+			BindingFlags.Instance |
+			BindingFlags.NonPublic;
+
+		private static readonly FieldInfo m_info = typeof( EditorApplication )
+			.GetField( "projectWasLoaded", BINDING_ATTR );
+
+		public static UnityAction projectWasLoaded
+		{
+			get => m_info.GetValue( null ) as UnityAction;
+			set
+			{
+				var functions = m_info.GetValue( null ) as UnityAction;
+				functions += value;
+				m_info.SetValue( null, functions );
+			}
+		}
+	}
+
+	[InitializeOnLoad]
+	internal static class MuteWhenLoadedProject
+	{
+		static MuteWhenLoadedProject()
+		{
+			EditorApplicationUtility.projectWasLoaded += () => EditorUtility.audioMasterMute = true;
+		}
+	}
+}
---
+++ README.md
@@ -0,0 +1,15 @@
+# Uni Editor Muter
+
+Unity プロジェクト起動時に Mute Audio をオフにするエディタ拡張
+
+![](https://img.shields.io/badge/Unity-2018.4%2B-red.svg)
+![](https://img.shields.io/badge/.NET-4.x-orange.svg)
+[![](https://img.shields.io/github/license/baba-s/uni-editor-muter.svg)](https://github.com/baba-s/uni-editor-muter/blob/master/LICENSE)
+
+## インストール
+
+```json
+"com.baba_s.uni-editor-muter": "https://github.com/baba-s/uni-editor-muter.git",
+```
+
+manifest.json に上記の記述を追加します  
